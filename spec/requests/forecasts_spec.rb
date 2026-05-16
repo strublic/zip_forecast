a560bdf1d253be91325f105154dd22a9eae25351
Gledson Strublic <gledson.strublic@iob.com.br>
2026-05-16 20:13:26 -0300

    Refactor test environment configuration and add ForecastFetcher specs

--- spec/requests/forecasts_spec.rb
+++ spec/requests/forecasts_spec.rb
@@ -1,11 +1,41 @@
 require 'rails_helper'
 
-RSpec.describe "Forecasts", type: :request do
-  describe "GET /index" do
-    it "returns http success" do
-      get "/forecasts/index"
-      expect(response).to have_http_status(:success)
+RSpec.describe 'Forecasts', type: :request do
+  describe 'POST /forecasts' do
+    it 'renders forecast result' do
+      allow(ForecastFetcher).to receive(:call).and_return(
+        {
+          zip: '10002',
+          current_temp: 22,
+          high: 26,
+          low: 18,
+          condition: 'Sunny',
+          from_cache: false
+        }
+      )
+
+      post forecasts_path, params: {
+        forecast: {
+          address: 'Cherry Street'
+        }
+      }
+
+      expect(response).to have_http_status(:ok)
+      expect(response.body).to include('10002')
+    end
+
+    it 'handles invalid address' do
+      allow(ForecastFetcher)
+        .to receive(:call)
+        .and_raise(InvalidAddressError)
+
+      post forecasts_path, params: {
+        forecast: {
+          address: 'bad address'
+        }
+      }
+
+      expect(response).to have_http_status(:unprocessable_entity)
     end
   end
-
 end
---
+++ spec/services/forecast_fetcher_spec.rb
@@ -0,0 +1,81 @@
+require 'rails_helper'
+
+RSpec.describe ForecastFetcher do
+  let(:address) { 'Cherry Street' }
+  let(:zip) { '10002' }
+
+  let(:weather_response) do
+    {
+      'current' => {
+        'temp_c' => 22,
+        'condition' => {
+          'text' => 'Sunny'
+        }
+      },
+      'forecast' => {
+        'forecastday' => [
+          {
+            'day' => {
+              'maxtemp_c' => 26,
+              'mintemp_c' => 18
+            }
+          }
+        ]
+      }
+    }
+  end
+
+  before do
+    Rails.cache.clear
+  end
+
+  describe '.call' do
+    it 'returns fresh forecast on cache miss' do
+      allow(Geocoding::AddressResolver)
+        .to receive(:call)
+        .with(address)
+        .and_return(zip)
+
+      weather_client = instance_double(Weather::Client)
+
+      allow(Weather::Client)
+        .to receive(:new)
+        .and_return(weather_client)
+
+      allow(weather_client)
+        .to receive(:current)
+        .with(zip)
+        .and_return(weather_response)
+
+      result = described_class.call(address)
+
+      expect(result[:zip]).to eq(zip)
+      expect(result[:current_temp]).to eq(22)
+      expect(result[:high]).to eq(26)
+      expect(result[:low]).to eq(18)
+      expect(result[:condition]).to eq('Sunny')
+      expect(result[:from_cache]).to eq(false)
+    end
+
+    it 'returns cached forecast on cache hit' do
+      cached_data = {
+        zip: zip,
+        current_temp: 22,
+        high: 26,
+        low: 18,
+        condition: 'Sunny'
+      }
+
+      allow(Geocoding::AddressResolver).to receive(:call).and_return(zip)
+                                                         .to receive(:call)
+        .and_return(zip)
+
+      Rails.cache.write("forecast:#{zip}", cached_data)
+
+      result = described_class.call(address)
+
+      expect(result[:from_cache]).to eq(true)
+      expect(result[:zip]).to eq(zip)
+    end
+  end
+end
